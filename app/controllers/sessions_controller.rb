94fe4215c421722c8a066f32760390c691e2084a
Emily Zhao <tong.zhao.92@gmail.com>
2018-07-25 15:07:28 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,6 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(name: params[:name])
-    binding.pry
     if user.password == params[:password]
       session[:user_id] = user.id
       redirect_to user_path(user)
