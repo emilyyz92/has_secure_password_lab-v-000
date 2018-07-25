2a232ac00c32d57c6e4fc972afacfb70b619a415
Emily Zhao <tong.zhao.92@gmail.com>
2018-07-25 14:28:48 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(user_params).save
     if @user.valid?
       @user.save
       redirect_to user_path(@user)
