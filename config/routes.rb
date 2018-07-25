2320e245f6eaf63cd1e9b0432270bd8c0cfa4bd9
Emily Zhao <tong.zhao.92@gmail.com>
2018-07-25 14:57:37 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :show]
-  get '/signup' => "users#new"
-  post '/signup' => "users#create"
+  # get '/signup' => "users#new"
+  # post '/signup' => "users#create"
   get '/' => "users#welcome"
   get '/login' => "sessions#new"
   post '/login' => "sessions#create"
